ad3e8085acdf09215fcb04d1278bb42b59b77c55
aimad moustajil <moustajil.dev@gmail.com>
2025-08-03 15:25:03 +0100

    a

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:aivoicehelper/Pallet.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +12,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title:Text("Aimad Helper"),
+        leading: Icon(Icons.menu),
+         
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  width: 120,
+                  height: 120,
+                  margin: EdgeInsets.all(4),
+                  child: Image.asset("assets/images/ai-voice-generator.png"),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                margin: EdgeInsets.symmetric(horizontal: 40,vertical: 30),
+                child: Text("Good Morning , What Task Can i do for you today ?",style: TextStyle(color: Pallete.mainFontColor,fontSize: 25),
+              )
+          )],
+          )
+        ],
+      ),
       
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aivoicehelper/Pallet.dart';
 import 'package:aivoicehelper/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,appBarTheme: AppBarTheme(backgroundColor: Pallete.whiteColor)
+      ),
       title: 'Ai Helper',
       home: HomePage(),
     );
